b48d765b3f2a0b1e35937da24a78ecb7cb26563d
Ubuntu <ubuntu@ip-10-164-0-147.ec2.internal>
2013-07-10 13:26:50 +0000

    Grader can now accept a url argument

--- grader.js
+++ grader.js
@@ -24,8 +24,10 @@ References:
 var fs = require('fs');
 var program = require('commander');
 var cheerio = require('cheerio');
+var rest = require('restler');
 var HTMLFILE_DEFAULT = "index.html";
 var CHECKSFILE_DEFAULT = "checks.json";
+var URL_DEFAULT = '';
 
 var assertFileExists = function(infile) {
     var instr = infile.toString();
@@ -36,16 +38,42 @@ var assertFileExists = function(infile) {
     return instr;
 };
 
-var cheerioHtmlFile = function(htmlfile) {
-    return cheerio.load(fs.readFileSync(htmlfile));
+var assertUrlExists = function(url) {
+    var instr = url.toString();
+    // It seems like we are mixing synchronous and asynchronous APIs in a way
+    // that can cause problems.
+    // When I include this test, it seems to interfere with processUrlData.
+    /*
+    rest.head(instr).on('complete', function(result) {
+	if (result instanceof Error) {
+            console.log("%s does not exist. Exiting.", instr);
+            process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	}
+    });
+    */
+    return instr;
+};
+
+var processUrlData = function(url) {
+    rest.get(url).on('complete', function(result) {
+	if (result instanceof Error) {
+            console.log("Could not load %s. Exiting.", url);
+            process.exit(1); // http://nodejs.org/api/process.html#process_process_exit_code
+	}
+	finishMain(result);
+    });
+};
+
+var getHtmlFileData = function(htmlfile) {
+    return fs.readFileSync(htmlfile);
 };
 
 var loadChecks = function(checksfile) {
     return JSON.parse(fs.readFileSync(checksfile));
 };
 
-var checkHtmlFile = function(htmlfile, checksfile) {
-    $ = cheerioHtmlFile(htmlfile);
+var checkData = function(data, checksfile) {
+    $ = cheerio.load(data);
     var checks = loadChecks(checksfile).sort();
     var out = {};
     for(var ii in checks) {
@@ -55,20 +83,31 @@ var checkHtmlFile = function(htmlfile, checksfile) {
     return out;
 };
 
+
 var clone = function(fn) {
     // Workaround for commander.js issue.
     // http://stackoverflow.com/a/6772648
     return fn.bind({});
 };
 
+var finishMain = function(data) {
+    var checkJson = checkData(data, program.checks);
+    var outJson = JSON.stringify(checkJson, null, 4);
+    console.log(outJson);
+};
+
 if(require.main == module) {
     program
         .option('-c, --checks <check_file>', 'Path to checks.json', clone(assertFileExists), CHECKSFILE_DEFAULT)
         .option('-f, --file <html_file>', 'Path to index.html', clone(assertFileExists), HTMLFILE_DEFAULT)
+        .option('-u, --url <url>', 'URL to check', clone(assertUrlExists), URL_DEFAULT)
         .parse(process.argv);
-    var checkJson = checkHtmlFile(program.file, program.checks);
-    var outJson = JSON.stringify(checkJson, null, 4);
-    console.log(outJson);
+    if (program.url) {
+	processUrlData(program.url);
+    } else {
+	var data = getHtmlFileData(program.file);
+	finishMain(data);
+    }
 } else {
     exports.checkHtmlFile = checkHtmlFile;
 }
